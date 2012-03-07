a006326f097e31006d2f092b9aa1ccaaee9ec86b
David Siegel <david.siegel@artcom.de>
2012-03-07 03:40:34 +0100

    decided to go with cleanest implementation

--- priority_queue.js
+++ priority_queue.js
@@ -2,136 +2,54 @@ var max_first = exports.max_first = function max_first(a, b) { return a - b; }
   , min_first = exports.min_first = function min_first(a, b) { return b - a; }
   ;
 
-var PriorityQueue = exports.PriorityQueue = function PriorityQueue(compare) {
-  if (!(this instanceof PriorityQueue)) return new PriorityQueue(compare);
+exports.PriorityQueue = function PriorityQueue(compare, queue) {
+  if (!(this instanceof PriorityQueue)) return new PriorityQueue(compare, queue);
 
   compare = compare || max_first;
+  queue   = queue   || [];
 
-  var a = [], n = 0;
+  function swap(i, j) { var t = queue[i]; queue[i] = queue[j]; queue[j] = t; }
 
-  function down(parent) {
-    var new_element = a[parent]
-      , child = 2 * parent
-      ;
-    while (child <= n) {
-      if (child < n) {
-        if (compare(a[child + 1], a[child]) > 0) {
-          ++child;
-        }
-      }
-      if (compare(new_element, a[child]) < 0) {
-        a[parent] = a[child];
-        parent = child; 
-        child = 2 * parent;
-      } else {
-        break;
-      }
+  function heapify(i) {
+    var l = queue.length, x;
+    while (true) {
+      x = i;
+      if (left(i)  < l && compare(queue[left(i)],  queue[x]) > 0) x = left(i);
+      if (right(i) < l && compare(queue[right(i)], queue[x]) > 0) x = right(i);
+      if (x === i) break;
+      swap(i, x);
+      i = x;
     }
-    a[parent] = new_element;
   }
 
-  function up(child) {
-    var new_element = a[child]
-      , parent = Math.floor(child/2)
-      ;
-    while (parent >= 1) {
-      if (compare(a[parent], new_element) < 0) {
-        a[child] = a[parent];
-        child = parent;
-        parent = Math.floor(child/2);
-      } else {
-        break;
+  this.push = function push(/* element, ... */) {
+    var i = queue.length, e = i + arguments.length, j;
+    queue.push.apply(queue, arguments);
+    for (; i < e; ++i) {
+      j = i;
+      for(j = i; j >= 0 && compare(queue[j], queue[parent(j)]) > 0; j = parent(j)) {
+        swap(j, parent(j));
       }
     }
-    a[child] = new_element;
   }
 
-  this.__defineGetter__('length', function() { return n });
-
-  this.insert = function insert(/* elements */) {
-    var i = n, j;
-    for (j = 0; j < arguments.length; ++j) {
-      a[++n] = arguments[j];
+  function remove(i) {
+    var t = queue[i], b = queue.pop();
+    if (queue.length > 0) {
+      queue[i] = b;
+      heapify(i);
     }
-    for (++i; i <= n; ++i) {
-      up(i);
-    }
+    return t;
   }
 
-  this.peek = function peek() {
-    if (n >= 1) return a[1];
-  }
+  this.pop = function pop() { return remove(0); }
+  this.__defineGetter__('length', function length() { return queue.length });
 
-  this.pop = function pop() {
-    if (n < 1) {
-      throw new Error('can not pop() empty priority queue');
-    }
-    var result = a[1];
-    a[1] = a[n--];
-    down(1);
-    return result;
-  }
-
+  for (var i = queue.length / 2 - 1; i >= 0; --i) { heapify(i) }
 }
 
+function left(i)  { return 2 * i + 1 }
+function right(i) { return 2 * i + 2 }
+function parent(i) { return Math.floor((i + 1) / 2) - 1 }
 
-//==============================================================================
-// Test
-//==============================================================================
 
-
-if (module === require.main) {
-  var assert  = require('assert')
-    , vows    = require('vows')
-    , numbers = []
-    ;
-  for (var i = 0; i < 100; ++i) {
-    numbers.push(Math.random());
-  }
-
-  var suite = vows.describe('priority queue').addBatch(
-  { 'A new priority queue': 
-    { topic: PriorityQueue
-    , 'is a PriorityQueue instance': isPriorityQueue
-    , 'is empty': function(q) { assert.strictEqual(q.length, 0) }
-    }
-  , 'A filled priority queue': queueTest(42, 5, 23)
-  , 'An inverted priority queue': queueTest(42, 5, 23, min_first)
-  , 'A longer priority queue': queueTest.apply(null, numbers)
-  });
-  suite.reporter = require('vows/lib/vows/reporters/spec');
-  suite.run();
-
-  function isPriorityQueue(q) {
-    assert.instanceOf(q, PriorityQueue);
-  }
-
-  function pqueueWithItems() {
-    var items = arguments;
-    return function() {
-      var q = new PriorityQueue();
-      q.insert.apply(q, items);
-      return q;
-    }
-  }
-
-  function queueTest() {
-    var items = Array.prototype.slice.call(arguments)
-      , cmp = (typeof items[items.length - 1] === 'function') ? items.pop() : max_first
-      , sorted = items.sort(cmp)
-      , ctx = {}
-      ;
-    ctx.topic = function() {
-      var q = new PriorityQueue(cmp);
-      q.insert.apply(q, items);
-      return q;
-    }
-    ctx['returns the items in order'] = function(q) {
-      while (sorted.length) {
-        assert.strictEqual(q.length, sorted.length);
-        assert.strictEqual(q.pop(), sorted.pop());
-      }
-    }
-    return ctx;
-  }
-}
